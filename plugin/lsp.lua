7ed5fe1f721bbc8259631c06813bf115b9e48293
SerhiiCho <serhiicho@protonmail.com>
2025-03-30 19:16:57 +0300

    Disabled lsp for now

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -6,31 +6,31 @@ vim.filetype.add({
     },
 })
 
-local lsp = require("lspconfig")
+-- local lsp = require("lspconfig")
 
 -- Store the client ID to manage the server instance
-local textwire_client = nil
+-- local textwire_client = nil
 
 -- LSP setup
-lsp.textwire.setup({
-    cmd = { "/Users/serhiichornenkyi/www/open/textwire/lsp/main" },
-    on_attach = function(client)
-        textwire_client = client.id
-    end,
-    -- Only start the server for Textwire files
-    filetypes = { "textwire" },
-    -- Prevent the server from starting automatically
-    autostart = false,
-})
+-- lsp.textwire.setup({
+--     cmd = { "/Users/serhiichornenkyi/www/open/textwire/lsp/main" },
+--     on_attach = function(client)
+--         textwire_client = client.id
+--     end,
+--     -- Only start the server for Textwire files
+--     filetypes = { "textwire" },
+--     -- Prevent the server from starting automatically
+--     autostart = false,
+-- })
 
 -- Create autocmd to stop the server when Neovim exits
-vim.api.nvim_create_autocmd("VimLeavePre", {
-    callback = function()
-        if textwire_client then
-            local client = vim.lsp.get_client_by_id(textwire_client)
-            if client then
-                client.stop()
-            end
-        end
-    end,
-})
+-- vim.api.nvim_create_autocmd("VimLeavePre", {
+--     callback = function()
+--         if textwire_client then
+--             local client = vim.lsp.get_client_by_id(textwire_client)
+--             if client then
+--                 client.stop()
+--             end
+--         end
+--     end,
+-- })
