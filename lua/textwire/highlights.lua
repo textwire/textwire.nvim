203a086e642773043df3b8680b47ef6f3ace2d03
SerhiiCho <serhiicho@protonmail.com>
2025-05-01 21:54:13 +0300

    refactor: rename the function

--- lua/textwire/highlights.lua
+++ lua/textwire/highlights.lua
@@ -4,29 +4,28 @@ local utils = require("textwire.utils")
 
 --- Move highlight files from the plugin directory to the user's config.
 --- @return nil
-function highlights.move()
-    local queries_dir = utils.plugin_path() .. "/queries"
-    local dest_dir = vim.fn.stdpath("config") .. "/queries/textwire"
+function highlights.refresh_files()
+	local queries_dir = utils.plugin_path() .. "/queries"
+	local dest_dir = vim.fn.stdpath("config") .. "/queries/textwire"
 
-    if not utils.is_dir(dest_dir) then
-        vim.fn.mkdir(dest_dir, "p")
-    end
+	if not utils.is_dir(dest_dir) then
+		vim.fn.mkdir(dest_dir, "p")
+	end
 
-    local files = vim.fn.globpath(queries_dir, "*", 0, 1)
+	local files = vim.fn.globpath(queries_dir, "*", 0, 1)
 
-    -- Copy files from queries_dir to dest_dir
-    for _, file in ipairs(files) do
-        local file_name = vim.fn.fnamemodify(file, ":t")
-        local dest_file = dest_dir .. "/" .. file_name
+	-- Copy files from queries_dir to dest_dir
+	for _, file in ipairs(files) do
+		local file_name = vim.fn.fnamemodify(file, ":t")
+		local dest_file = dest_dir .. "/" .. file_name
 
-        -- Delete the file if it already exists
-        if vim.fn.filereadable(dest_file) then
-            vim.fn.delete(dest_file)
-        end
+		-- Delete the file if it already exists
+		if vim.fn.filereadable(dest_file) then
+			vim.fn.delete(dest_file)
+		end
 
-        vim.fn.system({ "cp", file, dest_file })
-    end
+		vim.fn.system({ "cp", file, dest_file })
+	end
 end
 
 return highlights
-
--- lua/textwire/init.lua
+++ lua/textwire/init.lua
@@ -2,7 +2,7 @@ local textwire = {}
 
 local deprecation_msg = [[
 textwire.load_highlights() is deprecated and will be removed in the next major
-version update. Use textwire.build() instead.
+version update. Use textwire.nuild() instead.
 
 Go into your plugins/textwire.lua config file and rename load_highlights()
 to build() to hide this warning message.
@@ -12,18 +12,18 @@ to build() to hide this warning message.
 --- @return nil
 --- @deprecated This function is deprecated. Use `textwire.build()` instead.
 function textwire.load_highlights()
-    require("textwire.highlights").move()
+	require("textwire.highlights").move()
 
-    vim.notify(deprecation_msg, vim.log.levels.WARN, {
-        title = "Deprecation Warning",
-        timeout = 10000,
-    })
+	vim.notify(deprecation_msg, vim.log.levels.WARN, {
+		title = "Deprecation Warning",
+		timeout = 10000,
+	})
 end
 
 --- Install proper highlights for Textwire
 --- @return nil
 function textwire.build()
-    require("textwire.highlights").move()
+	require("textwire.highlights").refresh_files()
 end
 
 return textwire
