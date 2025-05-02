b2aa20d6cf9499496e1d059ed0fc094e2c508b8f
SerhiiCho <serhiicho@protonmail.com>
2025-05-02 13:30:25 +0300

    refactor: change tabs to spaces

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,23 +1,23 @@
 -- Add file type detection for .tw and .tw.html
 vim.filetype.add({
-	extension = {
-		tw = "textwire",
-		["tw.html"] = "textwire",
-	},
+    extension = {
+        tw = "textwire",
+        ["tw.html"] = "textwire",
+    },
 })
 
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
 parser_config.textwire = {
-	install_info = {
-		url = "https://github.com/textwire/tree-sitter-textwire",
-		files = { "src/parser.c", "src/scanner.c" },
-		branch = "master",
-		-- if stand-alone parser without npm dependencies
-		generate_requires_npm = false,
-		-- if directory contains pre-generated src/parser.c
-		requires_generate_from_grammar = true,
-	},
+    install_info = {
+        url = "https://github.com/textwire/tree-sitter-textwire",
+        files = { "src/parser.c", "src/scanner.c" },
+        branch = "master",
+        -- if stand-alone parser without npm dependencies
+        generate_requires_npm = false,
+        -- if directory contains pre-generated src/parser.c
+        requires_generate_from_grammar = true,
+    },
 }
 
 require("textwire.lsp").attach()
