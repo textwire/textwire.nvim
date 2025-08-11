6910f4f9d41f0ef7329fe8dc9fac2d875a38d2cf
SerhiiCho <serhiicho@protonmail.com>
2025-08-11 14:49:46 +0300

    BREAKING CHANGE: remove load_highlights func, use 'build' instead

--- lua/textwire/init.lua
+++ lua/textwire/init.lua
@@ -1,25 +1,5 @@
 local textwire = {}
 
-local deprecation_msg = [[
-textwire.load_highlights() is deprecated and will be removed in the next major
-version update. Use textwire.nuild() instead.
-
-Go into your plugins/textwire.lua config file and rename load_highlights()
-to build() to hide this warning message.
-]]
-
---- Load the highlights (syntax highlighting) for Textwire.
---- @return nil
---- @deprecated This function is deprecated. Use `textwire.build()` instead.
-function textwire.load_highlights()
-    require("textwire.highlights").move()
-
-    vim.notify(deprecation_msg, vim.log.levels.WARN, {
-        title = "Deprecation Warning",
-        timeout = 10000,
-    })
-end
-
 --- Install proper highlights for Textwire
 --- @return nil
 function textwire.build()
